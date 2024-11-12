c98ecf502009413b9c1757ad79ac7efd0ae1df02
Raymond Leon <raymondleon@raymondleon.ca>
2024-11-12 14:59:20 -0500

    Updates documentation

--- documentation/demos/test/index.js
+++ documentation/demos/test/index.js
@@ -1,15 +1,29 @@
-import {ParserGenerator, TreeViewer} from '../spg.js'
-let parserGenerator = new ParserGenerator()
+// import {ParserGenerator, TreeViewer} from '../spg.js'
+// let parserGenerator = new ParserGenerator()
 
-let parserDefinition = 
+// let parserDefinition = 
+// `string literal
+//  Hello, world.`
+
+// let parser = parserGenerator.generateParser(parserDefinition)
+
+// let testProgram = 'Hello, world. Magic.'
+
+// let output = parser.parse(testProgram)
+
+// let treeViewer = new TreeViewer()
+// treeViewer.display('text', output)
+
+import {ParserGenerator, TreeViewer} from 'simple-parser-generator'
+let parserGenerator = new ParserGenerator()
+let parserSpecification = 
 `string literal
  Hello, world.`
 
-let parser = parserGenerator.generateParser(parserDefinition)
+let parser = parserGenerator.generateParser(parserSpecification)
 
-let testProgram = 'Hello, world. Magic.'
-
-let output = parser.parse(testProgram)
+let inputString = 'Hello, world.'
+let output = parser.parse(inputString)
 
 let treeViewer = new TreeViewer()
 treeViewer.display('text', output)
